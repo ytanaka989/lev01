b410c605dcd6b4a4eb71f1fb3f53bb3295c65cdb
ytanaka989 <22011212yt@tama.ac.jp>
2023-02-24 23:49:27 +0900

    ブログ投稿一覧の表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,6 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-    return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        return view('posts/index')->with(['posts' => $post->get()]);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,20 +4,17 @@
         <meta charset="utf-8">
         <title>Blog</title>
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     </head>
-    <body>
+    <body class="antialiased">
         <h1>Blog Name</h1>
         <div class='posts'>
-            @foreach ($posts as $post)
+            @foreach($posts as $post)
                 <div class='post'>
                     <h2 class='title'>{{ $post->title }}</h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
-            @endforeach
-        </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
+                @endforeach
+            </div>
     </body>
 </html>
