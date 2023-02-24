c2d730969b5cbf22a2d56590e744943d6befaa9c
ytanaka989 <22011212yt@tama.ac.jp>
2023-02-24 19:11:57 +0900

    ページネーション追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,8 +9,8 @@ class Post extends Model
 {
     use HasFactory;
     
-    public function getByLimit(int $limit_count = 10)
+    public function getPaginateByLimit(int $limit_count = 5)
 {
-    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
 }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,20 +3,21 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
-
         <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body class="antialiased">
+    <body>
         <h1>Blog Name</h1>
         <div class='posts'>
             @foreach ($posts as $post)
-            <div class='post'>
-                <h2 class='title'>{{ $post->title }}</h2>
-                <p class='body'>{{ $post->body }}</p>
-            </div>
+                <div class='post'>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
             @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
