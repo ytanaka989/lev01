5b85e1a9c16e30e7373e47984e25e081a97dfe17
ytanaka989 <22011212yt@tama.ac.jp>
2023-03-02 20:11:32 +0900

    ブログ投稿の削除

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -32,10 +32,15 @@ class PostController extends Controller
     return view('posts/edit')->with(['post' => $post]);
     }
     public function update(PostRequest $request, Post $post)
-{
+    {
     $input_post = $request['post'];
     $post->fill($input_post)->save();
 
     return redirect('/posts/' . $post->id);
-}
+     }
+     public function delete(Post $post)
+     {
+    $post->delete();
+    return redirect('/');
+     }
 }
